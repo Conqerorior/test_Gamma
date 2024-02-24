300eb887da433020563e22e9966cbd05c2eb2dc1
Серей Овинцев <ozonti@mail.ru>
2024-02-24 20:17:41 +0300

    Создание приложения posts

--- test_Gamma/test_Gamma/settings.py
+++ test_Gamma/test_Gamma/settings.py
@@ -21,7 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.PostsConfig'
+
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
