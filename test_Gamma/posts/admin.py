23aef1ea6818f629a40de980aac7ac6ac8e716d4
Серей Овинцев <ozonti@mail.ru>
2024-02-26 15:03:32 +0300

    Создание админ панели для приложения posts

--- test_Gamma/posts/admin.py
+++ test_Gamma/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Letter
+
+
+@admin.register(Letter)
+class LetterAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "sender_full_name",
+        "recipient_full_name",
+    )
+    list_display_links = ("id", "sender_full_name", "recipient_full_name")
+    list_filter = ("sender_full_name",)
+    search_fields = ("sender_full_name",)
