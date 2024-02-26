ae407fcf48190812b491624ccee55e053d5045d2
Серей Овинцев <ozonti@mail.ru>
2024-02-26 15:02:59 +0300

    Изменение полей для индексов Городов

--- test_Gamma/posts/models.py
+++ test_Gamma/posts/models.py
@@ -12,43 +12,36 @@ class Letter(models.Model):
     VALUABLE_LETTER = 2
     EXPRESS_LETTER = 3
     LETTER_TYPES = (
-        (LETTER, 'Письмо'),
-        (REGISTERED_LETTER, 'Заказное письмо'),
-        (VALUABLE_LETTER, 'Ценное письмо'),
-        (EXPRESS_LETTER, 'Экспресс-письмо'),
+        (LETTER, "Письмо"),
+        (REGISTERED_LETTER, "Заказное письмо"),
+        (VALUABLE_LETTER, "Ценное письмо"),
+        (EXPRESS_LETTER, "Экспресс-письмо"),
     )
 
-    sender_full_name = models.CharField(
-        max_length=128, verbose_name='ФИО отправителя'
-    )
+    sender_full_name = models.CharField(max_length=128, verbose_name="ФИО отправителя")
     recipient_full_name = models.CharField(
-        max_length=128, verbose_name='ФИО получателя'
+        max_length=128, verbose_name="ФИО получателя"
     )
-    departure_point = models.TextField(verbose_name='Пункт отправки')
-    destination_point = models.TextField(verbose_name='Пункт получения')
-    departure_index = models.PositiveSmallIntegerField(
-        default=0, verbose_name='Индекс места отправки'
+    departure_point = models.TextField(verbose_name="Пункт отправки")
+    destination_point = models.TextField(verbose_name="Пункт получения")
+    departure_index = models.PositiveIntegerField(
+        default=0, verbose_name="Индекс места отправки"
     )
-    destination_index = models.PositiveSmallIntegerField(
-        default=0, verbose_name='Индекс места получения'
+    destination_index = models.PositiveIntegerField(
+        default=0, verbose_name="Индекс места получения"
     )
     letter_type = models.PositiveSmallIntegerField(
-        default=LETTER, choices=LETTER_TYPES, verbose_name='Тип письма'
+        default=LETTER, choices=LETTER_TYPES, verbose_name="Тип письма"
     )
-    letter_weight = models.PositiveSmallIntegerField(
-        default=0, verbose_name='Вес письма'
-    )
+    letter_weight = models.PositiveIntegerField(default=0, verbose_name="Вес письма")
 
     class Meta:
-        verbose_name = 'Письма'
-        verbose_name_plural = 'Письма'
-        ordering = ('sender_full_name',)
+        verbose_name = "Письма"
+        verbose_name_plural = "Письма"
+        ordering = ("sender_full_name",)
 
     def __str__(self):
-        return (
-            f'Письмо от: {self.sender_full_name} '
-            f'для {self.recipient_full_name}'
-        )
+        return f"Письмо от: {self.sender_full_name} " f"для {self.recipient_full_name}"
 
 
 class Package(models.Model):
@@ -63,43 +56,36 @@ class Package(models.Model):
     INTERNATIONAL_PACKAGE = 4
     EXPRESS_PACKAGE = 5
     PACKAGE_TYPES = (
-        (SMALL_PACKAGE, 'Мелкий пакет'),
-        (PACKAGE, 'Посылка'),
-        (FIRST_CLASS_PACKAGE, 'Посылка 1 класса'),
-        (VALUABLE_PACKAGE, 'Ценная посылка'),
-        (INTERNATIONAL_PACKAGE, 'Посылка международная'),
-        (EXPRESS_PACKAGE, 'Экспресс-посылка'),
+        (SMALL_PACKAGE, "Мелкий пакет"),
+        (PACKAGE, "Посылка"),
+        (FIRST_CLASS_PACKAGE, "Посылка 1 класса"),
+        (VALUABLE_PACKAGE, "Ценная посылка"),
+        (INTERNATIONAL_PACKAGE, "Посылка международная"),
+        (EXPRESS_PACKAGE, "Экспресс-посылка"),
     )
 
-    sender_full_name = models.CharField(
-        max_length=255, verbose_name='ФИО отправителя'
-    )
+    sender_full_name = models.CharField(max_length=255, verbose_name="ФИО отправителя")
     recipient_full_name = models.CharField(
-        max_length=255, verbose_name='ФИО получателя'
+        max_length=255, verbose_name="ФИО получателя"
     )
-    departure_point = models.TextField(verbose_name='Пункт отправки')
-    destination_point = models.TextField(verbose_name='Пункт получения')
-    departure_index = models.PositiveIntegerField(
-        verbose_name='Индекс места отправки'
-    )
+    departure_point = models.TextField(verbose_name="Пункт отправки")
+    destination_point = models.TextField(verbose_name="Пункт получения")
+    departure_index = models.PositiveIntegerField(verbose_name="Индекс места отправки")
     destination_index = models.PositiveIntegerField(
-        verbose_name='Индекс места получения'
+        verbose_name="Индекс места получения"
     )
-    phone = PhoneNumberField(region='RU', verbose_name='Телефон для извещения')
-    package_type = models.CharField(
-        default=SMALL_PACKAGE, choices=PACKAGE_TYPES, verbose_name='Тип посылки'
+    phone = PhoneNumberField(region="RU", verbose_name="Телефон для извещения")
+    package_type = models.PositiveSmallIntegerField(
+        default=SMALL_PACKAGE, choices=PACKAGE_TYPES, verbose_name="Тип посылки"
     )
     payment_amount = models.DecimalField(
-        max_digits=6, decimal_places=2, verbose_name='Сумма платежа'
+        max_digits=6, decimal_places=2, verbose_name="Сумма платежа"
     )
 
     class Meta:
-        verbose_name = 'Посылка'
-        verbose_name_plural = 'Посылки'
-        ordering = ('sender_full_name',)
+        verbose_name = "Посылка"
+        verbose_name_plural = "Посылки"
+        ordering = ("sender_full_name",)
 
     def __str__(self):
-        return (
-            f'Посылка от: {self.sender_full_name} '
-            f'для {self.recipient_full_name}'
-        )
+        return f"Посылка от: {self.sender_full_name} " f"для {self.recipient_full_name}"
