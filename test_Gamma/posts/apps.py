6a71cc1277afdc2afe083e2cc99be94c8a4a96a3
Серей Овинцев <ozonti@mail.ru>
2024-02-26 14:27:58 +0300

    Добавление описание приложения

--- test_Gamma/posts/apps.py
+++ test_Gamma/posts/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "posts"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'posts'
+    verbose_name = 'Отправка Посылок и Писем'
