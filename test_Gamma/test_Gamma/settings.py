36b246935e458ba33b293e3bf0af704234968d30
Серей Овинцев <ozonti@mail.ru>
2024-02-26 15:43:36 +0300

    Изменение настроек rest_framework

--- test_Gamma/test_Gamma/settings.py
+++ test_Gamma/test_Gamma/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "phonenumber_field",
     "api.apps.ApiConfig",
     "posts.apps.PostsConfig",
