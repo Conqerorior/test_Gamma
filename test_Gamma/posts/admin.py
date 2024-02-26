4a9f4fb35f1d02de0f54f6fec3ec8f0b234cf7c1
Серей Овинцев <ozonti@mail.ru>
2024-02-26 17:38:49 +0300

    Создание админки для Package и Letter

--- test_Gamma/posts/admin.py
+++ test_Gamma/posts/admin.py
@@ -1,15 +1,40 @@
 from django.contrib import admin
 
-from .models import Letter
+from .models import Letter, Package
 
 
 @admin.register(Letter)
 class LetterAdmin(admin.ModelAdmin):
     list_display = (
-        "id",
-        "sender_full_name",
-        "recipient_full_name",
+        'id',
+        'sender_full_name',
+        'recipient_full_name',
+        'departure_point',
+        'destination_point',
+        'departure_index',
+        'destination_index',
+        'letter_type',
+        'letter_weight',
     )
-    list_display_links = ("id", "sender_full_name", "recipient_full_name")
-    list_filter = ("sender_full_name",)
-    search_fields = ("sender_full_name",)
+    list_display_links = ('id', 'sender_full_name', 'recipient_full_name')
+    list_filter = ('sender_full_name',)
+    search_fields = ('sender_full_name',)
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'sender_full_name',
+        'recipient_full_name',
+        'departure_point',
+        'destination_point',
+        'departure_index',
+        'destination_index',
+        'phone',
+        'package_type',
+        'payment_amount',
+    )
+    list_display_links = ('id', 'sender_full_name', 'recipient_full_name')
+    list_filter = ('sender_full_name',)
+    search_fields = ('sender_full_name',)
