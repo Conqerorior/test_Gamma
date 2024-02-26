b0cab26105ef4fa021e7430b10e8e5ed7389a5b6
Серей Овинцев <ozonti@mail.ru>
2024-02-26 14:47:46 +0300

    Создание маршрутов для letter

--- test_Gamma/posts/models.py
+++ test_Gamma/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Letters(models.Model):
+class Letter(models.Model):
     """
     Модель для представления информации о письмах.
     """
